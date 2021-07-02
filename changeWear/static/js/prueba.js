d72d7ccb7c46883f9165845fd224c63c1ac3ba35
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-07-01 22:20:40 -0400

    Update prueba.js

--- changeWear/static/js/prueba.js
+++ changeWear/static/js/prueba.js
@@ -2,11 +2,10 @@ var dataResult = null;
 categoria = 'HM'
 
 $.ajax({
-    url: `http://127.0.0.1:8000/api/categoria/HM`,
+    url: `http://127.0.0.1:8000/api/categoria/${categoria}`,
     dataType: 'json'
 }).done((data) => {
     dataResult = data;
-    console.log(data)
     dataResult.forEach(element => {
         console.log(element);
     });
