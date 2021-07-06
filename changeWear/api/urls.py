277b2ac1f5a5e3be4a6f7ff5aec90da0cce1abcf
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-07-06 19:47:46 -0400

    login

--- changeWear/api/urls.py
+++ changeWear/api/urls.py
@@ -3,9 +3,11 @@ from django.urls.conf import re_path
 from rest_framework import urlpatterns
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
+from .viewLogin import login
 
 # Url vemos endpoint de la api
 urlpatterns = [
     path('productos/', productos_ser),
     path('categoria/<pk>', categoria_producto, name='categoria_producto'),
+    path('login', login, name="login"),
 ]
